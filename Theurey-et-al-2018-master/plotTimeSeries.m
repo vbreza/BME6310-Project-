64d82f0b3a09724e5ed2eabe7fe19bb3ee1a4232
Natasha <nlclaxton19@ufl.edu>
2019-12-01 18:26:18 -0500

    Plots desired figured with correct size and axis labels

--- Theurey-et-al-2018-master/plotTimeSeries.m
+++ Theurey-et-al-2018-master/plotTimeSeries.m
@@ -142,7 +142,7 @@ if plotOutput == 1
     [OCR_converted,Output27Conv,Output5Conv,...
     J_ATP_total,prop_cyto,prop_mito,convert] = unitsConvert(OutputAll,dim);
 
-    subplot(3,3,2), hold on
+   hold on
     for i = 1:numSims
         Output4Conv = OutputAll(:,4,i)*convert;
         plot(tt(4:stepsize*600:end),Output4Conv(4:stepsize*600:end)); 
@@ -152,9 +152,9 @@ if plotOutput == 1
         meanOutput4Conv = meanOutput(:,4)*convert;
         plot(tt(4:stepsize*600:end),meanOutput4Conv(4:stepsize*600:end),'k','LineWidth',2);
     end
-    axis([0 60 0 20e-12])
+    axis([0 60 0 25e-12])
     xlabel('Time (min)')
-    ylabel('J_C4: OCR (mol O_2/min/ug protein)')
+    ylabel('OCR (mol O_2/min/ug protein)')
 %     
 %     subplot(3,3,5), hold on
 %     for i = 1:numSims
